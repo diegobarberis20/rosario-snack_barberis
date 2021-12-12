fcd2415584aec7eb4bc39a2a31de054251238b1a
Diego Barberis <diegobarberis20@gmail.com>
2021-12-12 14:55:32 -0300

    Desafio 2 - V1.01

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,18 @@ import logoRosarioSnacks from '../assets/logoRosarioSnacks.jpg';
 
 function NavBar(){
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
                 <img src={logoRosarioSnacks} alt="" className='logo'/>      
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
+                <h3 className="titulo">Rosario Snacks</h3>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Productos</a>
-                    <a class="nav-link" href="#">Conocenos</a>
-                    <a class="nav-link" href="#">Contacto</a>
+                <div className="collapse navbar-collapse text-light" id="navbarNavAltMarkup">
+                <div className="navbar-nav text-light">
+                    <a className="nav-link active" aria-current="page" href="#">Productos</a>
+                    <a className="nav-link" href="#">Conocenos</a>
+                    <a className="nav-link" href="#">Contacto</a>
                 </div>
                 </div>
             </div>
